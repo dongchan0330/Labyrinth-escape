87f43cb9f7548ca68d51e5ea49a652b1c80cc1d8
dongchan0330 <dongchan0330@naver.com>
2022-06-08 13:05:12 +0900

    Update Script

---
+++ Script/boss.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class boss : Enemy
+{
+    public GameObject missile;
+    public Transform missileA;
+    public Transform missileB;
+
+    Vector3 lookVec;
+    Vector3 tauntVec;
+    public bool isLook;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        meshs = GetComponentsInChildren<MeshRenderer>();
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+        nav.isStopped = true;
+        StartCoroutine(Think());
+    }
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            StopAllCoroutines();
+            return;
+        }
+        if (isLook)
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            lookVec = new Vector3(h, 0, v) * 5f;
+            transform.LookAt(Target.position + lookVec);
+        }
+        else
+            nav.SetDestination(tauntVec);
+    }
+
+    IEnumerator Think()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        int ranAction = Random.Range(0, 5);
+        switch (ranAction)
+        {
+            case 0:
+            case 1:
+                StartCoroutine(MissileShot());
+                break;
+
+            case 2:
+            case 3:
+                StartCoroutine(RockShot());
+                break;
+
+            case 4:
+                StartCoroutine(Taunt());
+                break;
+        }
+    }
+    IEnumerator MissileShot()
+    {
+        anim.SetTrigger("doShot");
+        yield return new WaitForSeconds(0.2f);
+        GameObject instantMissileA = Instantiate(missile, missileA.position, missileA.rotation);
+        BossMissile bossMissileA = instantMissileA.GetComponent<BossMissile>();
+        bossMissileA.target = Target;
+
+        yield return new WaitForSeconds(0.3f);
+        GameObject instantMissileB = Instantiate(missile, missileB.position, missileB.rotation);
+        BossMissile bossMissileB = instantMissileB.GetComponent<BossMissile>();
+        bossMissileB.target = Target;
+
+        yield return new WaitForSeconds(2f);
+
+        StartCoroutine(Think());
+    }
+    IEnumerator RockShot()
+    {
+        isLook = false;
+        anim.SetTrigger("doBigShot");
+        Instantiate(bullet, transform.position, transform.rotation);
+        yield return new WaitForSeconds(3f);
+
+        isLook = true;
+        StartCoroutine(Think());
+    }
+    IEnumerator Taunt()
+    {
+        tauntVec = Target.position + lookVec;
+
+        isLook = false;
+        nav.isStopped = false;
+        boxCollider.enabled = false;
+        anim.SetTrigger("doTaunt");
+        yield return new WaitForSeconds(1.5f);
+        meleeArea.enabled = true;
+
+        yield return new WaitForSeconds(0.5f);
+        meleeArea.enabled = false;
+
+        yield return new WaitForSeconds(1f);
+        isLook = true;
+        nav.isStopped = true;
+        boxCollider.enabled = true;
+
+        StartCoroutine(Think());
+    }
+}
